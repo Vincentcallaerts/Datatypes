645791196367308c2adcdfe2c9005da30512050a
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 11:08:36 +0100

    Add project files.

---
+++ Oefening1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Oefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1, getal2, oplossing1, oplossing2, oplossing3;
+            double  oplossing4, oplossing5, oplossing6;
+
+            Console.Write("Wat is je eerste getal: ");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wat is je tweede getal: ");
+            getal2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(getal1 + " + " + getal2 + " = " + (oplossing1 = getal1 + getal2));
+            Console.WriteLine(getal1 + " - " + getal2 + " = " + (oplossing2 = getal1 - getal2));
+            Console.WriteLine(getal1 + " * " + getal2 + " = " + (oplossing3 = getal1 * getal2));
+            Console.WriteLine(getal1 + " / " + getal2 + " = " + (oplossing4 = Convert.ToDouble(getal1) / getal2));
+
+            oplossing5 = oplossing1 + oplossing2 + oplossing3 + oplossing4;
+            oplossing6 = oplossing1 * oplossing2 * oplossing3 * oplossing4;
+
+            Console.WriteLine($"{oplossing1} + {oplossing2} + {oplossing3} + {oplossing4} = {Math.Round(oplossing5, 2)}");
+            Console.WriteLine($"{oplossing1} * {oplossing2} * {oplossing3} * {oplossing4} = {Math.Round(oplossing6,2)}");
+
+            Console.ReadLine();
+        }
+    }
+}
