56b2fca8e07b390dd8db7283dba5b5c0d357e1e1
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 13:56:52 +0100

    Oefening 8

---
+++ Oefening 8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double getal;
+
+            Console.Write("Getal: ");
+            getal = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{getal} x {i+1} = {getal * (i+1)}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
