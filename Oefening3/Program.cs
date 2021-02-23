b134978f5b717b337b5a38cda8f957286fe92dce
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 11:38:20 +0100

    Oefening 3 klaar

---
+++ Oefening3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Oefening3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double far, cel;
+
+            Console.Write("Hoeveel grade Fahrenheit is het momenteel: ");
+            far = Convert.ToDouble(Console.ReadLine());
+            cel = Math.Round((far - 32) / 1.8);
+            Console.WriteLine($"Het is momenteel {cel} Celcius ");
+            Console.ReadLine();
+        }
+    }
+}
