acfbc8fd4ae2419b368d2a617948330b0cf5e0a5
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 15:48:18 +0100

    Recursief fibonacci

--- Oefening 10/Program.cs
+++ Oefening 10/Program.cs
@@ -7,20 +7,37 @@ namespace Oefening_10
     {
         static void Main(string[] args)
         {
-            int getal1 = 0, getal2 = 1, getal3, i, number;
 
+            int nummer;
             Console.Write("Hoeveel lang wil je je fibonacci rij : ");
-            number = Convert.ToInt32(Console.ReadLine());
-            Console.Write(getal1 + " " + getal2 + " ");
+            nummer = Convert.ToInt32(Console.ReadLine());
 
-            for (i = 2; i < number; ++i)
+            /* Normal loop
+            int getal1 = 0, getal2 = 1, getal3,
+            Console.Write(getal1 + " " + getal2 + " ");
+            for (i = 2; i < nummer; ++i)
             {
                 getal3 = getal1 + getal2;
                 Console.Write(getal3 + " ");
                 getal1 = getal2;
                 getal2 = getal3;
             }
+            */
 
+            void fibo(int eerstegetal, int tweedegetal, int teller, int lengte)
+            {
+
+                if (teller <= lengte)
+                {
+                    Console.Write(eerstegetal + " ");
+                    fibo(tweedegetal, eerstegetal + tweedegetal, teller+1, lengte);
+                }
+                
+
+            }
+
+            fibo(0,1,1,nummer);
+            
             Console.ReadLine();
         }
     }
