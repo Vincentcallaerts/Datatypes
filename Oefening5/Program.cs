7bc54b9e99c3238adfc6f2e186b8bb26e14f9fb1
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 13:11:43 +0100

    Oefening 5

---
+++ Oefening5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double getal1, getal2;
+
+            Console.Write("getal 1 :");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("getal 2 :");
+            getal2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Als je {getal1} deelt door {getal2}, dan is de rest {getal1%getal2}");
+            Console.ReadLine();
+        }
+    }
+}
