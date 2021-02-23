9db8059d3518e21c22b4d58beb1a9d6d1303caae
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 15:10:33 +0100

    fibonacci

---
+++ Oefening 10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Oefening_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1 = 0, getal2 = 1, getal3, i, number;
+
+            Console.Write("Hoeveel lang wil je je fibonacci rij : ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(getal1 + " " + getal2 + " ");
+
+            for (i = 2; i < number; ++i)
+            {
+                getal3 = getal1 + getal2;
+                Console.Write(getal3 + " ");
+                getal1 = getal2;
+                getal2 = getal3;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
