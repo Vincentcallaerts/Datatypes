92237074f9208c78e5287e150510b1a816978c06
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 14:24:00 +0100

    oefening 9

---
+++ Oefening 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double afstand, snelheid, tijdsduur;
+
+            Console.Write("Af te leggen afstand in km: ");
+            afstand = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Snelheid in km/h: ");
+            snelheid = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            tijdsduur = afstand/snelheid;
+          
+
+            Console.WriteLine($"De vrachtwagen zal nog {tijdsduur} uur moeten rijden.");
+           
+            Console.ReadLine();
+        }
+    }
+}
